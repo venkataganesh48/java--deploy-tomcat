fd6b9c80d3559a62533cbc43ba113fe02b1f7274
venkataganesh48 <saiganesh8075@gmail.com>
2025-07-07 20:17:30 +0530

    Create HelloServlet.java

---
+++ src/main/java/com/ecomm/HelloServlet.java
@@ -0,0 +1,14 @@
+package com.ecomm;
+
+import java.io.;
+import javax.servlet.;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+response.setContentType("text/html");
+PrintWriter out = response.getWriter();
+out.println("<h2>Hello from HelloServlet!</h2>");
+}
+}
